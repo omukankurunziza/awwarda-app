c17d0589dffb28b320af28965da8e4c44496de2b
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-04-04 13:29:28 +0200

    add tests.py

--- awwards/tests.py
+++ awwards/tests.py
@@ -5,34 +5,32 @@ class ProjectTestClass(TestCase):
 
     # Set up method
     def setUp(self):
-        self.indian= Project()
+        self.award= Project()
     # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.indian,Project))
+        self.assertTrue(isinstance(self.award,Project))
 
     # Testing Save Method
     def test_save_method(self):
-        self.indian.save_image()
+        self.award.save_image()
         description= Project.objects.all()
         self.assertTrue(len(description) > 0)
+
+        
+       
 class RatingTestClass(TestCase):
     # Set up method
     def setUp(self):
-        self.indian= Ratingt()
+        self.award= Rating()
     # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.indian,Rating))
+        self.assertTrue(isinstance(self.award,Rating))
 
-  
+   
 class ProfileTestClass(TestCase):
     # Set up method
     def setUp(self):
-        self.DSC_76721.JPG= Image( )
+        self.award= Profile( )
     # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.DSC_76721.JPG,Image))
-
-    # Testing Save Method
-    
-    def update_image(self, **kwargs):
-        self.objects.filter(id = self.pk).update(**kwargs) 
+        self.assertTrue(isinstance(self.award,Profile))

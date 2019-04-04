62c2023e4510d087507b7d178d497566340179e8
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-04-04 13:55:07 +0200

    add tests.py

--- awwards/tests.py
+++ awwards/tests.py
@@ -12,7 +12,7 @@ class ProjectTestClass(TestCase):
 
     # Testing Save Method
     def test_save_method(self):
-        self.tests.py.save_image()
+        self.gallery.save_image()
         description= Project.objects.all()
         self.assertTrue(len(description) > 0)
 
@@ -21,7 +21,7 @@ class ProjectTestClass(TestCase):
 class RatingTestClass(TestCase):
     # Set up method
     def setUp(self):
-        self.award= Rating()
+        self.= Rating()
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.award,Rating))
